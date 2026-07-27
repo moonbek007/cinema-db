c7ba9d00f02cfdf555db67b92e3520f74d82577e
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-27 13:45:17 +0500

    feat: Add explore page and implement ExploreDisplay component

---
+++ app/explore/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header/Header";
+import ExploreDisplay from "@/components/Explore/Explore";
+
+export default function Explore() {
+  return (
+    <>
+      <Header />
+      <ExploreDisplay />
+    </>
+  );
+}
