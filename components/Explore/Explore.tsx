5d3849efed187806377f74cdf8e2f03aa11fd27c
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-28 12:54:07 +0500

    refactor: Refactor ExploreDisplay and move it loading filters and loading filtered shows to utils

--- components/Explore/Explore.tsx
+++ components/Explore/Explore.tsx
@@ -9,12 +9,13 @@ import FiltersModal from "./FiltersSection/FiltersModal.tsx";
 import SearchBar from "./SearchBar.tsx";
 
 import {
-  defaultFilters,
   DropdownValues,
   FilterTypes,
   movies,
   QueryParams,
 } from "@/constants/constants.ts";
+import { loadFilters, loadFilteredShows } from "@/lib/utils.ts";
+
 import "../../css/filters.css";
 
 function ExploreDisplay() {
@@ -34,10 +35,9 @@ function ExploreDisplay() {
     });
   });
 
-  const [filters, setFilters] = useState(
-    loadFilters(queryFilters, defaultFilters),
-  );
+  const [filters, setFilters] = useState({ ...loadFilters(queryFilters) });
   const [filtersModalOpen, setFiltersModalOpen] = useState(false);
+
   const [filteredShows, setFilteredShows] = useState([
     ...loadFilteredShows(queryFilters),
   ]);
@@ -121,7 +121,7 @@ function ExploreDisplay() {
       newFilteredShows = movies;
     }
     setFilteredShows(newFilteredShows);
-    setFilters(loadFilters(filters, defaultFilters));
+    setFilters(loadFilters(filters));
 
     updateQueryParam(
       filters.reduce<Record<FilterTypes, string[]>>(
@@ -133,70 +133,6 @@ function ExploreDisplay() {
       ),
     );
   };
-
-  function loadFilteredShows(
-    filters: {
-      name: FilterTypes | QueryParams.SEARCH;
-      applied: string[];
-    }[],
-  ) {
-    let newFilteredShows: Show[] = [...movies];
-    let noFiltersApplied = true;
-
-    filters.forEach((filter) => {
-      if (!filter.applied.length) {
-        return;
-      }
-
-      noFiltersApplied = false;
-
-      newFilteredShows = [
-        ...newFilteredShows.filter((show) => {
-          switch (filter.name) {
-            case FilterTypes.RATING:
-              const rating = Math.floor(show.rating.average as number);
-              const filterRating = parseInt(filter.applied[0].substring(1));
-              return filterRating <= rating;
-
-            case FilterTypes.COUNTRY:
-              if (show.network?.country.name === DropdownValues.UNITED_STATES) {
-                return filter.applied.includes(DropdownValues.USA);
-              }
-
-              if (
-                show.network?.country.name === DropdownValues.UNITED_KINGDOM
-              ) {
-                return filter.applied.includes(DropdownValues.UK);
-              }
-
-              return filter.applied.includes(
-                (!!show.network && show.network?.country.name) as string,
-              );
-
-            case FilterTypes.STATUS:
-              return filter.applied.includes(show.status);
-
-            case FilterTypes.LANGUAGE:
-              return filter.applied.includes(show.language);
-
-            case FilterTypes.TYPE:
-              return filter.applied.includes(show.type);
-            case QueryParams.SEARCH:
-              return show.name
-                .toLowerCase()
-                .includes(filter.applied[0].toLowerCase());
-            default:
-              break;
-          }
-        }),
-      ];
-    });
-
-    if (noFiltersApplied) {
-      newFilteredShows = movies;
-    }
-    return newFilteredShows;
-  }
 
   const updateSearchQueryParam = (value: string) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -267,30 +203,3 @@ function ExploreDisplay() {
 }
 
 export default ExploreDisplay;
-
-function loadFilters(
-  filters: {
-    name: FilterTypes | QueryParams.SEARCH;
-    applied: string[];
-  }[],
-  defaultFilters: DefaultFiltersType,
-) {
-  const newFilters = { ...defaultFilters };
-  const filterNames = Object.keys(newFilters) as FilterTypes[];
-  filterNames.forEach((filter) => {
-    const appliedFilter = filters.find(
-      (loadedFilter) => loadedFilter.name === filter,
-    );
-    if (!!appliedFilter) {
-      newFilters[filter] = {
-        ...newFilters[filter],
-        value:
-          appliedFilter.applied.length > 1
-            ? `${appliedFilter.applied[0]} +${appliedFilter.applied.length - 1}`
-            : appliedFilter.applied[0] || DropdownValues.ALL,
-        applied: [...(appliedFilter?.applied as string[])],
-      };
-    }
-  });
-  return newFilters;
-}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,10 @@
 import {
+  defaultFilters,
   DropdownValues,
   FilterTypes,
   modalFilters,
+  movies,
+  QueryParams,
 } from "@/constants/constants";
 
 function loadFiltersModalFilters(filters: DefaultFiltersType) {
@@ -21,4 +24,92 @@ function loadFiltersModalFilters(filters: DefaultFiltersType) {
   return newFilters;
 }
 
-export { loadFiltersModalFilters };
+function loadFilters(
+  filters: {
+    name: FilterTypes | QueryParams.SEARCH;
+    applied: string[];
+  }[],
+) {
+  const newFilters = { ...defaultFilters };
+  const filterNames = Object.keys(newFilters) as FilterTypes[];
+  filterNames.forEach((filter) => {
+    const appliedFilter = filters.find(
+      (loadedFilter) => loadedFilter.name === filter,
+    );
+    if (!!appliedFilter) {
+      newFilters[filter] = {
+        ...newFilters[filter],
+        value:
+          appliedFilter.applied.length > 1
+            ? `${appliedFilter.applied[0]} +${appliedFilter.applied.length - 1}`
+            : appliedFilter.applied[0] || DropdownValues.ALL,
+        applied: [...(appliedFilter?.applied as string[])],
+      };
+    }
+  });
+  return newFilters;
+}
+
+function loadFilteredShows(
+  filters: {
+    name: FilterTypes | QueryParams.SEARCH;
+    applied: string[];
+  }[],
+) {
+  let newFilteredShows: Show[] = [...movies];
+  let noFiltersApplied = true;
+
+  filters.forEach((filter) => {
+    if (!filter.applied.length) {
+      return;
+    }
+
+    noFiltersApplied = false;
+
+    newFilteredShows = [
+      ...newFilteredShows.filter((show) => {
+        switch (filter.name) {
+          case FilterTypes.RATING:
+            const rating = Math.floor(show.rating.average as number);
+            const filterRating = parseInt(filter.applied[0].substring(1));
+            return filterRating <= rating;
+
+          case FilterTypes.COUNTRY:
+            if (show.network?.country.name === DropdownValues.UNITED_STATES) {
+              return filter.applied.includes(DropdownValues.USA);
+            }
+
+            if (show.network?.country.name === DropdownValues.UNITED_KINGDOM) {
+              return filter.applied.includes(DropdownValues.UK);
+            }
+
+            return filter.applied.includes(
+              (!!show.network && show.network?.country.name) as string,
+            );
+
+          case FilterTypes.STATUS:
+            return filter.applied.includes(show.status);
+
+          case FilterTypes.LANGUAGE:
+            return filter.applied.includes(show.language);
+
+          case FilterTypes.TYPE:
+            return filter.applied.includes(show.type);
+          case QueryParams.SEARCH:
+            return show.name
+              .toLowerCase()
+              .includes(filter.applied[0].toLowerCase());
+          default:
+            break;
+        }
+      }),
+    ];
+  });
+
+  if (noFiltersApplied) {
+    newFilteredShows = movies;
+  }
+  return newFilteredShows;
+}
+
+export { loadFiltersModalFilters, loadFilters, loadFilteredShows };
