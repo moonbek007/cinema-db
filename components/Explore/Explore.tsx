6d585602a7fcb7c2d07bd72903ebfc2a1a410003
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-07-29 16:09:10 +0500

    feat: Implement pagination to ExploreDisplay

--- components/Explore/Explore.tsx
+++ components/Explore/Explore.tsx
@@ -7,6 +7,7 @@ import { SlidersHorizontalIcon, StarIcon } from "lucide-react";
 import FilterResult from "./FilterResult.tsx";
 import FiltersModal from "./FiltersSection/FiltersModal.tsx";
 import SearchBar from "./SearchBar.tsx";
+import Pagination from "./Pagination.tsx";
 
 import {
   DropdownValues,
@@ -18,6 +19,8 @@ import {
   loadFilters,
   loadFilteredShows,
   getNumberOfFiltersApplied,
+  getPageDetails,
+  getPaginationIndecies,
 } from "@/lib/utils.ts";
 
 import "../../css/filters.css";
@@ -45,6 +48,20 @@ function ExploreDisplay() {
   const [filteredShows, setFilteredShows] = useState([
     ...loadFilteredShows(queryFilters),
   ]);
+
+  // Get page details from searchParams for the initial render
+  const initialPageDetails = getPageDetails(
+    searchParams.get(QueryParams.PAGE),
+    filteredShows.length,
+  );
+
+  const [page, setPage] = useState<PageType>({
+    ...initialPageDetails,
+  });
+  const [paginationIndecies, setPaginationIndecies] =
+    useState<PaginationIndeciesType>({
+      ...getPaginationIndecies(initialPageDetails, filteredShows.length),
+    });
 
   const updateQueryParam = (queryParams: QueryParamsType) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -130,6 +147,13 @@ function ExploreDisplay() {
       newFilteredShows = movies;
     }
 
+    // Update page details and pagination indecies
+    const newPageDetails = getPageDetails(null, newFilteredShows.length);
+    setPage({ ...newPageDetails });
+    setPaginationIndecies({
+      ...getPaginationIndecies(newPageDetails, newFilteredShows.length),
+    });
+
     setNumberOfFiltersApplied(getNumberOfFiltersApplied(filters));
     setFilteredShows(newFilteredShows);
     setFilters(loadFilters(filters));
@@ -147,6 +171,7 @@ function ExploreDisplay() {
           Rating: [],
           Type: [],
           Search: [],
+          Page: ["1"],
         },
       ),
     );
@@ -190,6 +215,21 @@ function ExploreDisplay() {
     ]);
   };
 
+  const handleChangePage = (pageNumber: number) => {
+    setPage(getPageDetails(`${pageNumber}`, filteredShows.length));
+    setPaginationIndecies(
+      getPaginationIndecies(
+        getPageDetails(`${pageNumber}`, filteredShows.length),
+        filteredShows.length,
+      ),
+    );
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(QueryParams.PAGE, `${pageNumber}`);
+
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="filters">
       <div className="filters__display">
@@ -227,17 +267,22 @@ function ExploreDisplay() {
         </div>
         <div className="separator"> </div>
         <div className="filters__display__results">
-          {filteredShows.slice(0, 20).map((show) => {
-            return (
-              <FilterResult
-                image={show.image.medium}
-                key={show.id}
-                link={show.url}
-                name={show.name}
-              />
-            );
-          })}
+          {filteredShows
+            .slice(paginationIndecies.start, paginationIndecies.end + 1)
+            .map((show) => {
+              return (
+                <FilterResult
+                  image={show.image.medium}
+                  key={show.id}
+                  link={show.url}
+                  name={show.name}
+                />
+              );
+            })}
         </div>
+        {page.totalPages > 1 && (
+          <Pagination page={page} changePage={handleChangePage} />
+        )}
       </div>
     </div>
   );
