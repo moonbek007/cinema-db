38332e18140a44aa6cc4cb24128af4923ba252a4
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-08-15 13:10:49 +0500

    feat: Change API link to production link and fix header z-index bug

--- constants/constants.ts
+++ constants/constants.ts
@@ -119,7 +119,7 @@ export const defaultPageValues: PageType = {
   totalPages: 1,
 };
 
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://moonbek007-api-cinema-db.netlify.app/api/";
 
 export const enum API_ENDPOINTS {
   COLLECTIONS = "/collections",
